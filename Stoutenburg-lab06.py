79af50bdc40fbe9de68cf13b978edeb55ed48fa7
rstouten <rstouten@uwyo.edu>
2024-10-16 21:49:54 +0000

    Submitting Lab 6

---
+++ Stoutenburg-lab06.py
@@ -0,0 +1,119 @@
+# Reuben Stoutenburg
+# UWYO COSC 1010
+# Submission Date 10/16/2024
+# Lab 06
+# Lab Section: 15
+# Sources, people worked with, help given to:
+# the max and min strings I used Code Academy 
+# your
+# comments
+# here
+
+
+random_string = """
+jppamiqxegokaizvkyawwurhewtcxohryzptznyuedhhmawpic
+pkzwuiorngdfcsgqnlyifzyaivehpiyszykqprbcsobygzhadd
+yfddbulxmcnyvqhesmnybyuhxjqqmhdxwhcselasiayqhctnlw
+hakethjahqnvjdowhlyzosemxkbenestxgvgncmffkcxldcmkl
+itclmqdhrbdgzwtvdxwedcknbyaecvttjphtxubvhwvcvjqayy
+almxuxjbcmznnzekptfzbldsjwpvringlmalwufvlppeiendur
+dyophftqjkghhncwxoksqaqnpueudpygiytqgpcgjqsjbtbpzi
+vaeczmyicnednjjoxkpnjmpfbgyjnbfjlweqqppodfxfzzwkuf
+rldgryyhceuikimoavosuzuozthmatcgxcmkxnaxmsevkcumby
+spiajlbycvrluxdkfavxidzalxuixqkxiybhfuqhcvmrhzbzse
+idjwgwdwgfkyreozkyoxdvhixfejxjfgkkgobescboyfshiovu
+fxdyvfsnmjzsphgmtldlaoehofcspzujghcdcxzggvunpbtglr
+topplmkviuewwpoaplmbpgejmymxbyzzwbnujrlysszmxkjerb
+zpiewqvgopvhmmcgwcyvxvwhdvfgsrybcozhdtwujhdbxzznkc
+ergcqbetpgwrejuluqfxchlihunzbcdwboysjqenjxzbgqbycx
+dybxpyztjyxpkqfvxullzkedpkjjobhymfinpvprxejktyrpai
+ehjgwahpquzcmvclatdfcmattavoehnhnzveoxwnmnptxbvxto
+gpcobgzdhsjevhcohkltftmrqkosknkxeylhqxkkctbnusijgr
+uvecpbqmylqdaohkfaqbgeokyyipumjuaaayikdzyxfrpaieyo
+uxiosjwioebsjtslblfurgcodtyaggzovzfnnyjngawiwbbtqi
+kqqhnkwheolpqzasmsmbxqkeiqvogquobphewznfsnlkkizhca
+cbiyvxpmjxywqvzqtshfvnfbusphggexfqzepsrduvtovdsknl
+ztyuwugprkhbmktfvrenbmqgdjwnkeugtojrpqfmjhtrlcqcpq
+pwsguedzgvktpwbqkhkueymjtxbvzmdfjopzkygujrjdtogssg
+cxczryuqhhgjlpultkoffescpzyjrfqqabnhkfdnhkylpjamxk
+uxidjkqdrkxqjqjtflebvwhcvqjciykzhrvppvxhvpedgznwty
+kujglixooczrhxziasjxddfcghzlwrqcyiilpruhdfvitewxzg
+dzcvmvnoskchscgoqfsojfvahlwkrslzeirlblseplcmpmbmum
+ibrdamvqfstydtjopdkdcbnnmpifxckozyxzluhcqbqtpismog
+ulufaajxvuizvdzioxfvypxovptkibcrjvfidomejknuggfrtp
+kptwffersvqjknemkejsgspckwqisdcliuezhbeqpwgrjcqajl
+huobykkbujmyuuinbwdklqfhvakyozzsxghfyownjjwqtkxgkf
+ipdbjzxfogozstfsektujsvklrvecditiectuvtfibohmxxzna
+cpqzeoburtquuizhypugnkvuwbdxnraareqkofhfjobrpcsuxq
+nbafxlkuafbfsiuyrxdusqyasqyrwhdjrukgxdackumvairlgn
+fjhenwbrdghbevgqbybpwncclolgqyuhallbqtzdywbvlzwtil
+jctmsxjortnxvlbhuhkblppewjhqjzxrwgftlturxjuwfoaqpp
+sgfnxwxolkbrpdmpniitoljzaxabgtnelrmryetxqypwrjdyjc
+zipwbdpbazxpesmrcfuikeamtlsrgxrhzfytecenyydeemrhxj
+gmdruhillntvpadzbroyygydpmonwuakruvxbdrqhtrjvoqsin
+gjbarzvuqplmsmbwtqfghteoknbxmaokwlqqfoblmzsxczjzfj
+mzmawtarjdtgongqqufhhdjwcinhlxcsgoltjycxrkloqozxoi
+crlfmgflzzxgiiliqlksxyaydsohhahzxtsufzppftvgbpsdlx
+ertfmbothijzrrdvfrnsohnwulcxvcvxngvmznhazxrgdsugij
+fracotpirvqemsiuualpkpvtmtgchmowkmvoolrjfblrtwkmtr
+xhawucytgwlahddkhxxfublukkdldpovqokntydhzzrxiisdwu
+ujrkoewqoflyebogbwgdhriwkkoiofwtjlhxxtmzkklzbcmxhv
+lrslowamkcwolbcgfkfciegdwqskuazxnycqkkggzsowcmafay
+ibmkdwkqmdkjesqnjiqpijixbwjhenmsrrlpcseliiajlvcaac
+zkdenxczyooloczcaahnkehbwimvieedpdlqfafbqvxvfmvabd
+"""
+random_string = random_string.replace("\n","") #remove all newline characters
+print(len(random_string)) # Print out the size for reference 
+
+# Above is a string with 2500 characters.
+# Create a program that goes through and counts the occurrence of each character, excluding \n using a  dictionary
+# Output each letter and its corresponding occurrence in alphabetical order
+# Output which letter occurred the most 
+# Output which letter occurred the least 
+# Output what the percentage of the string each character is, again in alphabetical
+
+#Tips and trick:
+# You can iterate through strings like you would a list
+# All characters are lowercase 
+# Each letter will be PAIRED with its corresponding value 
+# That is to say, this is a great use of dictionaries
+    # You will  need to add the letter to the dictionary on first occurrence 
+    # Then increment its corresponding count 
+
+
+#Load all the elements into a dictionary
+#Will need to first declare a dictionary 
+
+
+let_occ = {}
+
+for let in random_string:
+    if let in let_occ:
+        let_occ[let] += 1
+    else:
+        let_occ[let] = 1
+
+# Output: each letter and its corresponding occurrence in alphabetical order
+
+for let in sorted(let_occ):
+    print(let, let_occ[let])
+
+
+
+print("*"*75)
+# Output which letter occurred the most 
+
+most_occurred = max(let_occ, key = let_occ.get)
+least_occurred = min(let_occ, key = let_occ.get)
+
+print(f"The letter that occurred the most is {most_occurred}")
+print("*"*75)
+# Output which letter occurred the least 
+print(f"The letter that occurred the lest is {least_occurred}")
+print("*"*75)
+
+# Output what the percentage of the string each character is, again in alphabetical
+
+for let in sorted(let_occ):
+    per_string = (let_occ[let]/len(random_string)) * 100
+    print(let,":",per_string,"%")
